5613d1fb4b4ed403cf222377c5a024dc5e5014be
Max <max@maxleon.net>
2021-04-21 10:27:42 -0700

    - Refactored bytesToHex and hexToBytes

--- packages/client/lib/utils/bytesToHex.ts
+++ packages/client/lib/utils/bytesToHex.ts
@@ -1,12 +1,5 @@
-const HEX = '0123456789abcdef';
-
 export const bytesToHex = (bytes: Uint8Array) => {
-  let result = [];
-
-  for (let i = 0; i < bytes.length; i++) {
-    const v = bytes[i];
-    result.push(HEX[(v & 0xf0) >> 4] + HEX[v & 0x0f]);
-  }
-
-  return result.join('');
+  return Array.from(bytes)
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
 };
--- packages/client/lib/utils/hexToBytes.ts
+++ packages/client/lib/utils/hexToBytes.ts
@@ -1,9 +1,3 @@
-export const hexToBytes = (text: string) => {
-  let result: number[] = [];
-
-  for (let i = 0; i < text.length; i += 2) {
-    result.push(parseInt(text.substr(i, 2), 16));
-  }
-
-  return Uint8Array.from(result);
+export const hexToBytes = (hex: string) => {
+  return Buffer.from(hex, 'hex');
 };
