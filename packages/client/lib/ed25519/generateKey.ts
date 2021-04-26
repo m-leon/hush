143bc12ffc4aff2a6d6387105badce7a5dd0235a
Max <max@maxleon.net>
2021-04-26 13:31:10 -0700

    - Migrate to npm implementation of isaac - Refactor RNG away from class structure

--- packages/client/lib/ed25519/generateKey.ts
+++ packages/client/lib/ed25519/generateKey.ts
@@ -1,12 +1,11 @@
 import sodium from 'libsodium-wrappers';
 
-import { RNG } from '@/lib/rng';
+import { getBytes } from '@/lib/rng';
 
 export type SignKeyPair = ThenArg<ReturnType<typeof generateKey>>;
 
 export const generateKey = async (seed: string) => {
-  const rng = new RNG(seed);
   await sodium.ready;
-  const bytes = await rng.getBytes(sodium.crypto_sign_SEEDBYTES);
+  const bytes = await getBytes(sodium.crypto_sign_SEEDBYTES, seed);
   return sodium.crypto_sign_seed_keypair(bytes);
 };
--- packages/client/lib/xchacha20/generateKey.ts
+++ packages/client/lib/xchacha20/generateKey.ts
@@ -1,10 +1,9 @@
-import { RNG } from '@/lib/rng';
+import { getBytes } from '@/lib/rng';
 
 const KEY_LENGTH = 32;
 
 export type XChaCha20Key = ThenArg<ReturnType<typeof generateKey>>;
 
-export const generateKey = async (seed: string) => {
-  const rng = new RNG(seed);
-  return rng.getBytes(KEY_LENGTH);
+export const generateKey = (seed: string) => {
+  return getBytes(KEY_LENGTH, seed);
 };
